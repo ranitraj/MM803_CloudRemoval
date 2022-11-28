566cad96144b9866b6ea582d5961185b6961a24e
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2022-11-28 01:04:38 -0700

    Writing code to start training and saving checkpoints using utility method

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import config
 from data_mapper import DataMapper
 from model_generator import Generator
 from model_discriminator import Discriminator
-from utils import load_checkpoint
+from utils import load_checkpoint, save_checkpoint
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 torch.backends.cudnn.benchmark = True
+
+
+def start_training_dataset(discriminator, generator, train_dataloader, optimizer_discriminator,
+                           optimizer_generator, loss_l1, loss_bce, scalar_generator, scalar_discriminator):
+    pass
 
 
 def main():
@@ -34,7 +41,7 @@ def main():
     loss_bce = nn.BCEWithLogitsLoss()
     loss_l1 = nn.L1Loss()  # Works well with PatchGAN
 
-    # Loading configuration for Discriminator and Generator
+    # Starts loading checkpoint for Discriminator and Generator if FLAG_LOAD_MODEL is set to 'True' in config.py file
     if config.FLAG_LOAD_MODEL:
         load_checkpoint(
             config.CHECKPOINT_GENERATOR, generator, optimizer_generator, config.LEARNING_RATE,
@@ -55,6 +62,19 @@ def main():
     # Adding scalars for performing efficient steps while gradient scaling
     scalar_discriminator = torch.cuda.amp.GradScaler()
     scalar_generator = torch.cuda.amp.GradScaler()
+
+    # Starts training Generator and Discriminator using the training dataset
+    for cur_epoch in range(config.NUM_EPOCHS):
+        start_training_dataset(
+            discriminator, generator, train_dataloader, optimizer_discriminator,
+            optimizer_generator, loss_l1, loss_bce, scalar_generator, scalar_discriminator,
+        )
+
+        # Starts saving checkpoints if FLAG_SAVE_MODEL is set to 'True' in config.py file
+        if config.FLAG_SAVE_MODEL and cur_epoch % 5 == 0:
+            save_checkpoint(generator, optimizer_generator, filename=config.CHECKPOINT_GENERATOR)
+            save_checkpoint(discriminator, optimizer_discriminator, filename=config.CHECKPOINT_DISCRIMINATOR)
+
 
 if __name__ == "__main__":
     main()
