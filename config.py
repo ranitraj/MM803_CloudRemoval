55704e0a6c17a44ded775081c0db8c13e10b5fe2
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2022-11-27 21:48:32 -0700

    Updating config.py file with updated directory strructure and values

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import torch
-import torchvision.transforms as transforms
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/train"
-VAL_DIR = "data/val"
+
+TRAIN_DIR = "thin_cloud/training"
+VAL_DIR = "thin_cloud/validation"
+
 LEARNING_RATE = 2e-4
 BATCH_SIZE = 16
 NUM_WORKERS = 2
@@ -11,15 +12,11 @@ IMAGE_SIZE = 256
 CHANNELS_IMG = 3
 L1_LAMBDA = 100
 LAMBDA_GP = 10
+BETA_ONE = 0.5
+BETA_TWO = 0.999
 NUM_EPOCHS = 500
 LOAD_MODEL = False
 SAVE_MODEL = False
+
 CHECKPOINT_DISC = "disc.pth.tar"
 CHECKPOINT_GEN = "gen.pth.tar"
-
-# TODO: Can change
-all_transforms = transforms.Compose([transforms.Resize((256, 256)),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
-                                                          std=[0.2023, 0.1994, 0.2010])
-                                     ])
